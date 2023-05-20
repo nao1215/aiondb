c9a0aaf91d9ab797ad95725a69b8c36083ce35a1
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2023-05-21 01:36:28 +0900

    Add package comment

--- engine/parser/core/errors.go
+++ engine/parser/core/errors.go
@@ -1,3 +1,7 @@
+// Package core implements the common functions used in the parsing
+// of SQL queries. SQL queries have dialects specific to each RDBMS.
+// AION DB has been implemented to accommodate these dialects. This
+// package aims to minimize code duplication.
 package core
 
 import (
