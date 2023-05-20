a3077f0c5a5664e686cb2a66dbca661bf3324d63
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2023-05-21 01:32:36 +0900

    Initial commit

---
+++ engine/parser/core/errors.go
@@ -0,0 +1,20 @@
+package core
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ()
+
+// Wrap return wrapping error with message.
+// If e is nil, return new error with msg. If msg is empty string, return e.
+func Wrap(e error, message string) error {
+	if message == "" {
+		return e
+	}
+	if e == nil {
+		return errors.New(message)
+	}
+	return fmt.Errorf("%s: %w", message, e)
+}
